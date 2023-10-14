15160c0b073bdc1083780d315e6f88f109ad2fb9
Kenji <kenji@knji.note>
2023-10-14 16:09:08 -0300

    Arquivos FRontend

---
+++ frontend/src/index.ts
@@ -0,0 +1,3 @@
+const names = ['Kenji'];
+
+console.log(names);
---
+++ frontend/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  mode: process.env.NODE_ENV, 
+  entry: {
+    index: ['./src/index.ts'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'public', 'dist'),
+  },
+};
