192fe17660ace6dbbb97d07866ce7ceb88675133
Kenji <kenji@knji.note>
2023-10-15 22:59:53 -0300

    ainda nao ta funcionando essa porra

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,10 +2,12 @@ import express from 'express';
 
 // routes 
 import home from '../routes/home';
+import cadastrar from '../routes/cadastrar';
 
 const app = express();
 
 app.use('/', home);
+app.use('/', cadastrar);
 
 app.listen(3000, function(){
     console.log('Server running...');
