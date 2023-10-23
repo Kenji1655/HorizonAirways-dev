41847d3b50ac2aa8d40582171c3949ce345efae6
Kenji <kenji@knji.note>
2023-10-23 01:39:46 -0300

    ARQUIVO NOVO AE

---
+++ frontend/public/js/dashboard.js
@@ -0,0 +1,36 @@
+var abrirSidebar = false;
+var sidebar = document.getElementById("sidebar");
+
+function abrirSidebar() {
+  if (!abrirSidebar) {
+    sidebar.classList.add("sidebar-responsive");
+    abrirSidebar = true;
+  }
+}
+function fecharSidebar() {
+  if (abrirSidebar) {
+  }
+}
+
+function abrirPagina(pagina) {
+  window.location.href = pagina;
+}
+
+// Event listeners para os itens da sidebar
+document
+  .getElementById("aeronaves-sidebar")
+  .addEventListener("click", function () {
+    abrirPagina("aeronaves.html");
+  });
+
+document
+  .getElementById("aeroportos-sidebar")
+  .addEventListener("click", function () {
+    abrirPagina("aeroportos.html");
+  });
+
+document.getElementById("voos-sidebar").addEventListener("click", function () {
+  abrirPagina("voos.html");
+});
+
+
